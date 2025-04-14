a17d5883ddc65bdb23cc9dee5fadd7bc397e38cf
27breno <brenoferreira1327@gmail.com>
2025-04-14 16:11:47 -0300

    ultima vez tera que mexer manualmente de novo codigo todo

--- Frontend/vite-project/src/pages/Navbar.jsx
+++ Frontend/vite-project/src/pages/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './css/navbar.css';
+import '../css/navbar.css';
 
 function Navbar() {
   const [busca, setBusca] = useState('');
