713c08c41e1782a8534ac7f1be040d657eb30bdf
victorhrc242 <163910434+victorhrc242@users.noreply.github.com>
2025-04-14 14:07:21 -0300

    ola

--- dbRede/dbRede/obj/Debug/net9.0/dbRede.AssemblyInfo.cs
+++ dbRede/dbRede/obj/Debug/net9.0/dbRede.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dbRede")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f7b746420868993117cda6e6aee9d79005c8bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5d2c89ce16ebb52ef7ceaa01a523d7fb5a613d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dbRede")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dbRede")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
